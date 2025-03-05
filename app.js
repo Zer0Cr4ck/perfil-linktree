0a7c0a251a523466f055a953884ea7aa2ccfa514
joel torres <zer0cr4ckxd@gmail.com>
2025-03-05 18:07:55 -0500

    Trabajo terminado con js al 100%

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+let nombre = document.getElementById("name");
+
+nombre.innerText = "Joel Torres";
+
+let descripcion= document.getElementById("description");
+
+descripcion.innerText="Estudiante del Bootcamp de FullStack Developer en EnterTech.pe";
+
+
+let links = document.getElementById("links");
+
+
+links.innerHTML=`
+            <a href="#">Html</a>
+            <a href="#">Css</a>
+            <a href="#">Javascript</a>
+            <a href="#">Python</a>
+            <a href="#">React</a>
+
+`
+;
